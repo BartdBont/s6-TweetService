b3af0f91900fbccffdae62c96fd19afc835c7b64
BartdBont <bart_b@hotmail.nl>
2022-04-18 12:18:17 +0200

    Initial commit

---
+++ src/test/java/com/bart/TweetService/TweetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bart.TweetService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
