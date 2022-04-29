a9d42b58e707c468e6f53e9f9c7c5b84f9959377
BartdBont <bart_b@hotmail.nl>
2022-04-29 18:27:56 +0200

    enable eureka client

--- src/main/java/com/bart/TweetService/TweetServiceApplication.java
+++ src/main/java/com/bart/TweetService/TweetServiceApplication.java
@@ -3,8 +3,10 @@ package com.bart.TweetService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableEurekaClient
 public class TweetServiceApplication {
 
 	public static void main(String[] args) {
